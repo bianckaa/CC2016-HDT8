1a4070f178e136c2f0af2acccf96522ddcac1e74
bianckaa <bianckaraxon@gmail.com>
2025-04-10 21:05:03 -0600

    add Paciente.class

---
+++ src/Paciente.java
@@ -0,0 +1,11 @@
+public class Paciente {
+    private String nombre;
+    private String sintoma;
+    private String codigoEmergencia;
+
+    public Paciente(string nombre, String sintoma, String codigoEmergencia) {
+        this.nombre = nombre;
+        this.sintoma = sintoma;
+        this.codigoEmergencia = codigoEmergencia;
+    }
+}
